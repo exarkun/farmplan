6ba4de9d5c61b33168470629ca6ce303a1c2c43a
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2012-12-24 14:35:22 -0800

    Avoid running anything until the DOM is properly initialized

--- webui/cropplan.js
+++ webui/cropplan.js
@@ -1,164 +1,165 @@
 jQuery.noConflict();
+jQuery.ready(function() {
+    window.Crop = Backbone.Model.extend({
+        urlRoot: "../api/crops",
+        defaults: {
+            id: null,
+            name: "",
+            description: "",
+            picture: "",
+            yield_lbs_per_bed_foot: "",
+            rows_per_bed: "",
+            in_row_spacing: "",
+            row_feet_per_oz_seed: "",
+            harvest_weeks: ""}});
 
-window.Crop = Backbone.Model.extend({
-    urlRoot: "../api/crops",
-    defaults: {
-        id: null,
-        name: "",
-        description: "",
-        picture: "",
-        yield_lbs_per_bed_foot: "",
-        rows_per_bed: "",
-        in_row_spacing: "",
-        row_feet_per_oz_seed: "",
-        harvest_weeks: ""}});
+    window.CropPlan = Backbone.Collection.extend({
+        model: window.Crop,
+        url: "../api/crops"});
 
-window.CropPlan = Backbone.Collection.extend({
-    model: window.Crop,
-    url: "../api/crops"});
+    window.CropPlanView = Backbone.View.extend({
+        tagName: "ul",
 
-window.CropPlanView = Backbone.View.extend({
-    tagName: "ul",
+        initialize: function initialize() {
+            var self = this;
+            this.model.bind("reset", this.render, this);
+            this.model.bind("add", function addCrop(crop) {
+                jQuery(self.el).append(new CropPlanItemView({model: crop}).render().el);
+            });
+        },
 
-    initialize: function initialize() {
-        var self = this;
-        this.model.bind("reset", this.render, this);
-        this.model.bind("add", function addCrop(crop) {
-            jQuery(self.el).append(new CropPlanItemView({model: crop}).render().el);
-        });
-    },
+        render: function renderCropPlanView() {
+            /* XXX _? */
+            _.each(this.model.models, function renderOneCrop(crop) {
+                jQuery(this.el).append(new CropPlanItemView({model: crop}).render().el);
+            }, this);
+            return this;
+        }});
 
-    render: function renderCropPlanView() {
-        /* XXX _? */
-        _.each(this.model.models, function renderOneCrop(crop) {
-            jQuery(this.el).append(new CropPlanItemView({model: crop}).render().el);
-        }, this);
-        return this;
-    }});
-
-var t = jQuery("#template-crop-plan-item");
-var h = t.html();
-/* XXX _? */
-var tt = _.template(h);
-
-window.CropPlanItemView = Backbone.View.extend({
-    tagName: "li",
-    template: tt,
-
-    initialize: function initializeCropPlanItemView() {
-        this.model.bind("change", this.render, this);
-        this.model.bind("destroy", this.close, this);
-    },
-
-    render: function renderCropPlanItemView() {
-        jQuery(this.el).html(this.template(this.model.toJSON()));
-        return this;
-    },
-
-    close: function closeCropPlanItemView() {
-        jQuery(this.el).unbind();
-        jQuery(this.el).remove();
-    }});
-
-window.CropView = Backbone.View.extend({
+    var t = jQuery("#template-crop-plan-item");
+    var h = t.html();
     /* XXX _? */
-    template: _.template(jQuery("#template-crop-details").html()),
+    var tt = _.template(h);
 
-    initialize: function initializeCropView () {
-        this.model.bind("change", this.render, this);
-    },
+    window.CropPlanItemView = Backbone.View.extend({
+        tagName: "li",
+        template: tt,
 
-    render: function renderCropView() {
-        jQuery(this.el).html(this.template(this.model.toJSON()));
-        return this;
-    },
+        initialize: function initializeCropPlanItemView() {
+            this.model.bind("change", this.render, this);
+            this.model.bind("destroy", this.close, this);
+        },
 
-    events:{
-        "click .save": "saveCrop",
-        "click .delete": "deleteCrop"
-    },
+        render: function renderCropPlanItemView() {
+            jQuery(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        },
 
-    saveCrop: function saveCrop() {
-        var attributes = {};
-        for (var field in this.model.defaults) {
-            if (field == "id") {
-                continue;
+        close: function closeCropPlanItemView() {
+            jQuery(this.el).unbind();
+            jQuery(this.el).remove();
+        }});
+
+    window.CropView = Backbone.View.extend({
+        /* XXX _? */
+        template: _.template(jQuery("#template-crop-details").html()),
+
+        initialize: function initializeCropView () {
+            this.model.bind("change", this.render, this);
+        },
+
+        render: function renderCropView() {
+            jQuery(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        events:{
+            "click .save": "saveCrop",
+            "click .delete": "deleteCrop"
+        },
+
+        saveCrop: function saveCrop() {
+            var attributes = {};
+            for (var field in this.model.defaults) {
+                if (field == "id") {
+                    continue;
+                }
+                attributes[field] = jQuery("#" + field).val();
             }
-            attributes[field] = jQuery("#" + field).val();
-        }
-        this.model.set(attributes);
+            this.model.set(attributes);
 
-        if (this.model.isNew()) {
-            app.cropPlan.create(this.model);
-        } else {
-            this.model.save();
-        }
-        return false;
-    },
-
-    deleteCrop: function deleteCrop() {
-        this.model.destroy({
-            success: function deleteSuccess () {
-                alert('Crop deleted');
-                window.history.back();
+            if (this.model.isNew()) {
+                app.cropPlan.create(this.model, {wait: true});
+            } else {
+                this.model.save(null, null, {wait: true});
             }
-        });
-        return false;
-    },
+            return false;
+        },
 
-    close: function closeCrop() {
-        jQuery(this.el).unbind();
-        jQuery(this.el).empty();
-    }});
+        deleteCrop: function deleteCrop() {
+            this.model.destroy({
+                success: function deleteSuccess () {
+                    alert('Crop deleted');
+                    window.history.back();
+                }
+            });
+            return false;
+        },
 
-
-window.HeaderView = Backbone.View.extend({
-    template: _.template(jQuery('#template-header').html()),
-
-    initialize: function initializeHeader() {
-        this.render();
-    },
-
-    render: function renderHeader(eventName) {
-        jQuery(this.el).html(this.template());
-        return this;
-    },
-
-    events: {
-        "click .new": "newCrop"
-    },
-
-    newCrop: function newCrop(event) {
-        if (app.cropView) {
-            app.cropView.close();
-        }
-        app.cropView = new CropView({model: new Crop()});
-        jQuery('#content').html(app.cropView.render().el);
-        return false;
-    }});
+        close: function closeCrop() {
+            jQuery(this.el).unbind();
+            jQuery(this.el).empty();
+        }});
 
 
-var AppRouter = Backbone.Router.extend({
-    routes: {
-        "": "list",
-        "crops/:id": "crop-details"},
+    window.HeaderView = Backbone.View.extend({
+        template: _.template(jQuery('#template-header').html()),
 
-    initialize: function initializeRouter() {
-        jQuery('#header').html(new HeaderView().render().el);
-    },
+        initialize: function initializeHeader() {
+            this.render();
+        },
 
-    list: function list() {
-        this.cropPlan = new CropPlan();
-        this.cropPlanView = new CropPlanView({model: this.cropPlan});
-        this.cropPlan.fetch();
-        jQuery('#sidebar').html(this.cropPlanView.render().el);
-    },
+        render: function renderHeader(eventName) {
+            jQuery(this.el).html(this.template());
+            return this;
+        },
 
-    "crop-details": function crop_details(id) {
-        this.crop = this.cropPlan.get(id);
-        this.cropView = new CropView({model: this.crop});
-        jQuery('#content').html(this.cropView.render().el);
-    }});
+        events: {
+            "click .new": "newCrop"
+        },
 
-var app = new AppRouter();
-Backbone.history.start();
+        newCrop: function newCrop(event) {
+            if (app.cropView) {
+                app.cropView.close();
+            }
+            app.cropView = new CropView({model: new Crop()});
+            jQuery('#content').html(app.cropView.render().el);
+            return false;
+        }});
+
+
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            "": "list",
+            "crops/:id": "crop-details"},
+
+        initialize: function initializeRouter() {
+            jQuery('#header').html(new HeaderView().render().el);
+        },
+
+        list: function list() {
+            this.cropPlan = new CropPlan();
+            this.cropPlanView = new CropPlanView({model: this.cropPlan});
+            this.cropPlan.fetch();
+            jQuery('#sidebar').html(this.cropPlanView.render().el);
+        },
+
+        "crop-details": function crop_details(id) {
+            this.crop = this.cropPlan.get(id);
+            this.cropView = new CropView({model: this.crop});
+            jQuery('#content').html(this.cropView.render().el);
+        }});
+
+    var app = new AppRouter();
+    Backbone.history.start();
+});
